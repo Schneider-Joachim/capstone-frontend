4e992af75f50a707e48a928b538fbf9e4ed035ca
Schneider-Joachim <92768254+Schneider-Joachim@users.noreply.github.com>
2023-02-23 15:52:04 -0500

    push

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,3 +1,5 @@
+import  { Link } from "react-router-dom";
+
 const Cards = () => {
 	return (
 			<div className="container-sm">
@@ -8,7 +10,7 @@ const Cards = () => {
                 <div className="card-body">
                     <h5 className="card-title">EV Benefits</h5>
                     <p className="card-text">What are the benefits to owning an EV?</p>
-                    <a href="#" className="btn btn-primary">Learn</a>
+                    <Link to="/src/EVNpages/EVBenefits.js" className="btn btn-primary">Learn</Link>
                 </div>
             </div>
         </div>
@@ -19,7 +21,7 @@ const Cards = () => {
             <div className="card-body">
                 <h5 className="card-title">Charging</h5>
                 <p className="card-text">What should I know about charging my vehicle?</p>
-                <a href="#" className="btn btn-primary">Learn</a>
+                <Link to="/src/EVNpages/Charging.js" className="btn btn-primary">Learn</Link> 
             </div>
         </div>
     </div>
@@ -30,7 +32,7 @@ const Cards = () => {
             <div className="card-body">
                 <h5 className="card-title">Terms</h5>
                 <p className="card-text">Terms and Vocabulary to help you get a deeper understanding of the EV World</p>
-                <a href="#" className="btn btn-primary">Learn</a>
+                <Link to="/src/EVNpages/Terms.js" className="btn btn-primary">Learn</Link>
             </div>
         </div>
     </div> 
