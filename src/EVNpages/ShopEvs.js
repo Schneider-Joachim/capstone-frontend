ef78c95f0533a35206b01d1cb27200b6f4feaeb2
Schneider-Joachim <92768254+Schneider-Joachim@users.noreply.github.com>
2023-02-27 23:06:41 -0500

    shop

--- src/EVNpages/ShopEvs.js
+++ src/EVNpages/ShopEvs.js
@@ -1,11 +1,45 @@
 // functionallaity over css aesthethics* 
-
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ShopEvs = () => {
+
+const [make, ] = useState("");
+const [model,] = useState("");
+    
+
     return (
+        // created a selector, need to create useState 
         <div>
-            <h1>ShopEvs</h1>
+    <label for="make-select">Select a make:</label>
+    <select id="make-select">
+    <option value="">Choose a make</option>
+    <option value="Tesla">Tesla</option>
+    <option value="Ford">Ford</option>
+    </select>
+
+    <label for="make-select">Select a model:</label>
+    <select id="make-select">
+        <option value="">Choose a model</option>
+        <option value="Tesla">Model Y</option>
+        <option value="Tesla">Model X</option>
+        <option value="Ford">Mach E</option>
+    </select>
+
+    <table>
+    <thead>
+        <tr>
+        <th>Make</th>
+        <th>Model</th>
+        <th>Price</th>
+        <th>Range</th>
+        <th>Drivetrain</th>
+        </tr>
+    </thead>
+    <tbody id="model-details">
+    </tbody>
+    </table>
         </div>
     );
 };
